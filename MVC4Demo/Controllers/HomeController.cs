85f1371c9bea9552c22d25be1e5a195ab36af775
aa1356889 <310958153@qq.com>
2016-01-15 14:20:58 +0800

    [modefy]更改插件 实现一个页面多实例

---
+++ MVC4Demo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+
+namespace MVC4Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+
+    }
+}
